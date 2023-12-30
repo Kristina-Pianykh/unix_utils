ca4b52442369c3ce4201c5022206a43e81a57eef
Kristina Pianykh <kristinavrnrus@gmail.com>
2023-12-30 18:43:33 +0100

    chore: add quiet flag for diff exit(0)

--- test_compression.sh
+++ test_compression.sh
@@ -14,5 +14,5 @@ make wzip wunzip
 ./wzip $input_file > $cmp_file
 ./wunzip $cmp_file > $dcmp_file
 
-diff $input_file $dcmp_file
+diff -q $input_file $dcmp_file
 rm $input_file $cmp_file $dcmp_file
