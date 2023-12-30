8f3a655fae6e6fafff9db979ce3d86779f2a1e29
Kristina Pianykh <kristinavrnrus@gmail.com>
2023-12-30 18:44:32 +0100

    test: double reverse produce input file

--- test_reverse.sh
+++ test_reverse.sh
@@ -13,6 +13,7 @@ test_1() {
 	./reverse $input_file $input_file $input_file 2> /dev/null
 
 	if [ $? -eq 0 ]; then
+			echo "test_1 failed"
 			echo "./reverse $input_file $input_file $input_file should fail"
 			echo "too many arguments passed"
 			exit 1
@@ -30,14 +31,43 @@ test_2() {
 	diff $redirecred_output $output_file
 
 	if [ $? -ne 0 ]; then
+			echo "test_2 failed"
 			echo "./reverse $input_file and ./reverse $input_file $out_file differ"
+			rm $input_file $redirecred_output $output_file
 			exit 1
 	fi
 }
 
+
+test_3() {
+	# test if ./reverse $input_file | ./reverse 
+	# produces the same result as the original file $input_file
+	make $prg_name	
+	# Generate lines with random content
+	for i in {1..10}; do
+		# Use head -c to limit the length of each line
+		head -c $((RANDOM % 20 + 1)) /dev/urandom | tr -dc '[:print:]' >> "$input_file"
+		echo >> "$input_file"  # Add a newline after each line
+	done
+
+	./reverse $input_file | ./reverse > $redirecred_output
+	diff -q $redirecred_output $input_file
+
+	if [ $? -ne 0 ]; then
+			echo "test_3 failed"
+			echo "Double reverse of $input_file differ from the original file"
+			rm $input_file $redirecred_output
+			exit 1
+	fi
+}
+
+
 make $prg_name
 test_1
 test_2
+rm $input_file $redirecred_output $output_file
+test_3
+rm $input_file $redirecred_output
 
 if [ $? -eq 0 ]; then
 	echo ""
@@ -46,4 +76,3 @@ if [ $? -eq 0 ]; then
 fi
 
 make clean
-rm $input_file $redirecred_output $output_file
